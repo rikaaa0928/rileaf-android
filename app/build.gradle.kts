ea92544cc63ddc7904e33900d789b3a6adce40a3
rikaaa0928 <8528731+rikaaa0928@users.noreply.github.com>
2025-08-08 10:17:53 +0800

    skip minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            isShrinkResources = true
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
